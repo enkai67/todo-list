15cdfd367ed7ba498139fc8003776b887f1b2bde
enkai67 <nkai940372@gmail.com>
2023-08-15 15:08:00 +0800

    router init

--- app.js
+++ app.js
@@ -6,6 +6,34 @@ app.get('/', (req, res) =>{
   res.send('hello')
 })
 
+app.get('/todos',(req, res) => {
+  res.send('app get /todos page')
+})
+
+app.get('/todos/:id',(req, res) => {
+  res.send(`app get ${req.params.id} page`)
+})
+
+app.get('/todos/new',(req, res) => {
+  res.send('app get /todos/new page')
+})
+
+app.post('/todos',(req, res) => {
+  res.send('app post /todos page')
+})
+
+app.get('/todos/:id/edit',(req, res) => {
+  res.send(`app get ${req.params.id} page`)
+})
+
+app.put('/todos/:id',(req, res) => {
+  res.send(`app put ${req.params.id} page`)
+})
+
+app.delete('/todos/:id',(req, res) => {
+  res.send(`app delete ${req.params.id} page`)
+})
+
 app.listen(port, () =>{
   console.log(`app.js listening on port ${port}`)
 })
