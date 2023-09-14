41b443d1ebbc1d521757f287777fbe0f9ea89649
enkai67 <nkai940372@gmail.com>
2023-09-14 22:22:26 +0800

    add user-todo relationship migrations

--- models/todo.js
+++ models/todo.js
@@ -10,7 +10,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Todo.belongsTo(models.User), {
+        foreignKey: 'userId'
+      }
     }
   }
   Todo.init({
@@ -19,7 +21,11 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.BOOLEAN,
         defaultValue: false,
         allowNull: false
-      }
+      },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
     }, {
     sequelize,
     modelName: 'Todo',
