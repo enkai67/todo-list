7a58669a18cbf2524c1f2b3019e29349e352aaeb
enkai67 <nkai940372@gmail.com>
2023-09-16 15:47:42 +0800

    hashing password before inserting into users table

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 
 const passport = require('passport')
 const localStrategy = require('passport-local')
@@ -14,10 +15,16 @@ passport.use(new localStrategy({ usernameField: 'email' }, (username, password, 
     raw: true
   })
     .then((user) => {
-      if ( !user || user.password !== password){
+      if (!user){
         return done(null, false, { message: 'email或密碼輸入錯誤'})
       }
-      return done(null, user)
+      return bcrypt.compare(password, user.password)
+        .then((isMatch) => {
+          if (!isMatch){
+            return done(null, false, { message: 'email或密碼輸入錯誤'})
+          }
+          return done(null, user)
+        })
     })
     .catch((error) => {
       error.errorMessage = '登入失敗'
--- seeders/20230818060725-initial-data.js
+++ seeders/20230818060725-initial-data.js
@@ -1,20 +1,43 @@
 'use strict';
+const bcrypt = require('bcryptjs')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Todos',
-      Array.from({ length: 10 }).map((_, i) =>
-        ({
-          name: `todo-${i}`,
+    let transaction
+
+    try {
+      transaction = await queryInterface.sequelize.transaction()
+
+      const salt = await bcrypt.genSalt(10)
+      const hash = await bcrypt.hash('12345678', salt)
+
+      await queryInterface.bulkInsert('Users', [
+        {
+          id: 1,
+          name: 'root',
+          email: 'user@example.com',
+          password: hash,
           createdAt: new Date(),
           updatedAt: new Date()
-        })
-      )
-    )
-  },
+        }
+      ], {transaction})
+      await queryInterface.bulkInsert('Todos',
+        Array.from({ length: 10 }).map((_, i) =>
+          ({
+            name: `todo-${i}`,
+            userId: 1,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          })
+        ), {transaction})
+      await transaction.commit()
+    } catch(error){
+     if (transaction) await transaction.rollback()
+    }
+},
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Todos', null)
+    await queryInterface.bulkDelete('Users', null)
   }
 };
